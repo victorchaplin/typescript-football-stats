ee353ff872c14ffd35c1e76e361265e339f39daa
Victor Chaplin <victorchaplin@outlook.com>
2019-08-13 00:08:36 -0300

    finished first version. javascript style

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map(
+    (row: string): string[] => {
+      return row.split(',');
+    }
+  );
+
+let manUtdWins = 0;
+
+for (let match of matches) {
+  if (
+    (match[1] === 'Man United' && match[5] === 'H') ||
+    (match[2] === 'Man United' && match[5] === 'A')
+  ) {
+    manUtdWins++;
+  }
+}
+
+console.log(`Man United won ${manUtdWins} games`);
